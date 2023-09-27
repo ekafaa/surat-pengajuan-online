ee55dffbd8b90eba1833a0b32b873fcfeeb5447b
wisnucakraa <wisnucakraa@gmail.com>
2023-09-27 14:33:54 +0700

    update api url

--- utils/constant.js
+++ utils/constant.js
@@ -4,7 +4,7 @@ export const AUTH_HEADERS = {
   "Content-type": "application/json",
 };
 
-export const API_URL = "https://localhost:3000";
+export const API_URL = "https://surat-pengajuan-api.vercel.app/";
 
 export const allowedPathsUser = [
   "/",
